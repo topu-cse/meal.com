610480f0d88735f2f8a38d5ea28b7eb6c9330487
topu12345 <topu15-3604@diu.edu.bd>
2023-10-03 17:54:21 +0600

    nav banner

--- src/page/Home/Banner/Banner.js
+++ src/page/Home/Banner/Banner.js
@@ -1,12 +1,71 @@
 import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+// import './styles.css';
 
 import img1 from '../../../assets/back/banner-1.jpg'
 import img2 from '../../../assets/back/banner-2.jpg'
 const Banner = () => {
+
     return (
-        <div> 
-        
-</div>
+        <div>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                <SwiperSlide>
+                    <div className='z-1' >
+                        <div className='relative'>
+                            <div>
+                                {/* overlay */}
+                                <div className='absolute  w-full h-full bg-black/50 round-xl text-white  ' data-aos="fade-right"
+                                    data-aos-offset="300"
+                                    data-aos-easing="ease-in-sine" >
+                                    <div className='absolute top-[35%] lg:left-[20%] md:left-[20%] left-[10%] mr-[30px]' data-aos="fade-right">
+                                        <p className='text-green-500  font-comforter  md:text-[40px]'>Welcome </p>
+                                        <p className='text-[20px] lg:text-[40px md:text-[40px]'><span>TO HOT MEAL</span>  <br /> Reserve Your Table Now</p>
+
+                                        <button className='border-solid border-[1px] border-green-500 transition duration-700
+                              text-white absolute  rounded-none mt-2
+                              hover:bg-green-600 px-8 py-4'>EXPLORE PRODUCTS</button>
+
+                                    </div>
+                                </div>
+                                <img className='h-[460px] md:max-h-[500px]  w-full  object-cover  ' src={img1} alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='z-1'>
+                        <div className='relative'>
+                            <div>
+                                {/* overlay */}
+                                <div className='absolute  w-full h-full bg-black/50 round-xl text-white  '  data-aos="fade-right"
+                                    data-aos-offset="300"
+                                    data-aos-easing="ease-in-sine" >
+                                    <div className='absolute top-[35%] lg:left-[20%] md:left-[20%] left-[10%] mr-[30px]' data-aos="fade-right">
+                                        <p className='text-green-500  font-comforter  md:text-[40px]'>Welcome </p>
+                                        <p className='text-[20px] lg:text-[40px md:text-[40px]'><span>TO HOT MEAL</span>  <br /> Reserve Your Table Now</p>
+
+                                        <button className='border-solid border-[1px] border-green-500 transition duration-700
+                              text-white absolute  rounded-none mt-2
+                              hover:bg-green-600 px-8 py-4'>EXPLORE PRODUCTS</button>
+
+                                    </div>
+                                </div>
+                                <img className='h-[460px] md:max-h-[500px]  w-full  object-cover  ' src={img2} alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+            </Swiper>
+        </div>
 
     );
 };
--- src/page/Shared/Navber/Navber.js
+++ src/page/Shared/Navber/Navber.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { CgProfile } from "react-icons/cg";
-
+import logo from '../../../assets/icon/icon1.png'
 
 
 
 const Navber = () => {
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar  bg-black/80 text-white">
       <div className="navbar-start">
         {/* mobile */}
 
@@ -30,8 +30,9 @@ const Navber = () => {
             <label htmlFor="my-drawer" className="drawer-overlay"></label>
             <ul className="menu p-4 w-[250px] md:w-[400px] lg:w-[400px] min-h-full bg-white text-base-content">
               {/* Sidebar content here */}
-              <Link ><li><a>Sidebar Item 1</a></li></Link>
-              <li><a>Sidebar Item 2</a></li>
+              <li><Link to={'/'}>HOME</Link></li>
+
+              <li><Link>MENU</Link> </li>
 
 
             </ul>
@@ -41,68 +42,63 @@ const Navber = () => {
         {/* laptop */}
 
 
+        <div className='flex lg:ml-[80px]'>
+          <img className='w-[40px]' src={logo} alt="logo" />
+          <Link className=" mt-1 px-2 text-xl">MEAL</Link>
+        </div>
 
-        <a className=" normal-case text-xl">daisyUI</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><a>Item 1</a></li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </details>
-          </li>
-          <li><a>Item 3</a></li>
+        <ul className="menu-horizontal px-1">
+          <li className='px-2'><Link to={'/'}>HOME</Link></li>
+
+          <li className='px-2'><Link>MENU</Link> </li>
         </ul>
       </div>
 
-       <div className="navbar-end lg:pr-[100px] md:pr-[80px]">
-          <div className="flex">
+      <div className="navbar-end lg:pr-[100px] md:pr-[80px]">
+        <div className="flex">
 
-            {/* Cart */}
-            <div className="drawer drawer-end z-[40]">
-  <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content mt-4 px-4">
-    {/* Page content here */}
-    <label htmlFor="my-drawer-4" className="drawer-button indicator cursor-pointer">
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                    <span className="badge badge-sm indicator-item -z-10">0</span>
-    </label>
-  </div> 
-  <div className="drawer-side">
-    <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
-    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-      {/* Sidebar content here */}
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-    </ul>
-  </div>
-</div>
-
-            {/* Profile */}
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="rounded-full">
-                  <CgProfile size={30} />
-                </div>
+          {/* Cart */}
+          <div className="drawer drawer-end z-[40]">
+            <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content mt-4 px-4">
+              {/* Page content here */}
+              <label htmlFor="my-drawer-4" className="drawer-button indicator cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                <span className="badge badge-sm indicator-item -z-10">0</span>
               </label>
-              <ul tabIndex={0} className=" menu-sm dropdown-content  z-[1] p-2 shadow bg-gray-200  w-52">
-                <li>
-                  <Link to={'profile'}>
-                    Profile
-
-                  </Link>
-                </li>
-                <li><Link>Order</Link></li>
-                <li><Link to='/signin'>Sign In</Link></li>
+            </div>
+            <div className="drawer-side">
+              <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
+              <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                {/* Sidebar content here */}
+                <li><a>Sidebar Item 1</a></li>
+                <li><a>Sidebar Item 2</a></li>
               </ul>
             </div>
           </div>
+
+          {/* Profile */}
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="rounded-full">
+                <CgProfile size={30} />
+              </div>
+            </label>
+            <ul tabIndex={0} className=" menu-sm dropdown-content  z-[1] p-2 shadow bg-gray-200  w-52">
+              <li>
+                <Link to={'profile'}>
+                  Profile
+
+                </Link>
+              </li>
+              <li><Link>Order</Link></li>
+              <li><Link to='/signin'>Sign In</Link></li>
+            </ul>
+          </div>
         </div>
+      </div>
     </div>
   );
 };
